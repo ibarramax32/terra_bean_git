fbd33adce62e87aed0f797b0ed98d7d731413a53
Maximiliano Ibarra <74162048+ibarramax32@users.noreply.github.com>
2025-04-22 21:17:36 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,47 @@
 provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_s3_bucket" "beanstalk_bucket" {
-  bucket = "${var.project_name}-beanstalk-${random_id.bucket_id.hex}"
+  region = "us-east-1"
 }
 
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
+resource "aws_s3_bucket" "beanstalk_bucket" {
+  bucket = "mi-app-node-beanstalk-${random_id.bucket_id.hex}"
+}
+
 resource "aws_s3_bucket_object" "app_zip" {
   bucket = aws_s3_bucket.beanstalk_bucket.id
   key    = "app-version.zip"
-  source = "${path.module}/../app-version.zip"
-  etag   = filemd5("${path.module}/../app-version.zip")
+  source = "./../app-version.zip"
+  acl    = "private"
 }
 
 resource "aws_elastic_beanstalk_application" "app" {
-  name        = var.project_name
+  name        = "mi-app-node"
   description = "App desplegada con Terraform"
 }
 
+resource "aws_elastic_beanstalk_application_version" "app_version" {
+  name        = "v1-${timestamp()}"
+  application = aws_elastic_beanstalk_application.app.name
+  bucket      = aws_s3_bucket.beanstalk_bucket.bucket
+  key         = aws_s3_bucket_object.app_zip.key
+}
+
 resource "aws_elastic_beanstalk_environment" "env" {
-  name                = "${var.project_name}-env"
-  application         = aws_elastic_beanstalk_application.app.name
-  solution_stack_name = "64bit Amazon Linux 2 v5.8.4 running Node.js 18"
+  name                   = "mi-app-node-env"
+  application            = aws_elastic_beanstalk_application.app.name
+  solution_stack_name    = "64bit Amazon Linux 2023 v4.1.0 running Node.js 18"
+  tier                   = "WebServer"
+  version_label          = aws_elastic_beanstalk_application_version.app_version.name
+  wait_for_ready_timeout = "20m"
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance"
+  }
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
@@ -38,19 +54,4 @@ resource "aws_elastic_beanstalk_environment" "env" {
     name      = "NODE_ENV"
     value     = "production"
   }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "EnvironmentType"
-    value     = "SingleInstance"
-  }
-
-  version_label = aws_elastic_beanstalk_application_version.app_version.name
-}
-
-resource "aws_elastic_beanstalk_application_version" "app_version" {
-  name        = "v1-${timestamp()}"
-  application = aws_elastic_beanstalk_application.app.name
-  bucket      = aws_s3_bucket.beanstalk_bucket.id
-  key         = aws_s3_bucket_object.app_zip.id
 }
