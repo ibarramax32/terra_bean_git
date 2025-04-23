bdfce350cc4a8cc55785903b871d4aa796e89c30
Maximiliano Ibarra <74162048+ibarramax32@users.noreply.github.com>
2025-04-22 21:50:58 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -16,23 +16,22 @@ resource "aws_s3_object" "app_zip" {
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = "mi-app-node-env"
   application         = aws_elastic_beanstalk_application.app.name
-  environment_name    = "mi-app-node-env"
+  solution_stack      = "64bit Amazon Linux 2 v3.3.6 running Node.js 18"  # Definir la solución para Node.js
   version_label       = "v1"
+  cname_prefix        = "mi-app-node"  # Definir el dominio CNAME si es necesario
 
-  application_version {
-    bucket = aws_s3_bucket.beanstalk_bucket.bucket
-    key    = aws_s3_object.app_zip.key
-  }
-
-  settings {
+  setting {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
     value     = "4"
   }
 
-  settings {
+  setting {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
     value     = "1"
   }
+
+  application_version = aws_s3_object.app_zip.key  # Aquí pasamos la referencia al archivo zip como versión de la app
 }
+
