1eedbfd1385415818868c46822f449cf446a2282
Maximiliano Ibarra <74162048+ibarramax32@users.noreply.github.com>
2025-04-22 21:38:29 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -22,3 +22,35 @@ resource "aws_s3_object" "app_zip" {
 
 # Crear la aplicación de Elastic Beanstalk
 resource "aws_elastic_beanstalk_application" "app" {
+  name        = "mi-app-node"
+  description = "Aplicación Node.js para Beanstalk"
+}
+
+# Crear la versión de la aplicación en Elastic Beanstalk
+resource "aws_elastic_beanstalk_application_version" "app_version" {
+  name        = "app-version-${random_id.bucket_id.hex}"
+  application = aws_elastic_beanstalk_application.app.name
+  bucket      = aws_s3_bucket.beanstalk_bucket.id
+  key         = aws_s3_object.app_zip.key
+}
+
+# Crear el entorno de Elastic Beanstalk
+resource "aws_elastic_beanstalk_environment" "env" {
+  name                = "mi-app-node-env"
+  application         = aws_elastic_beanstalk_application.app.name
+  solution_stack_name = "64bit Amazon Linux 2 v5.10.0 running Node.js 18"
+  version_label       = aws_elastic_beanstalk_application_version.app_version.name
+
+  depends_on = [
+    aws_elastic_beanstalk_application.app,
+    aws_elastic_beanstalk_application_version.app_version
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  timeout {
+    create = "60m" # Ajusta este valor según lo necesites
+  }
+}
