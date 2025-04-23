e0c2c4331ccc3c6ea0d8c87c5f7f8f610338a7a6
Maximiliano Ibarra <74162048+ibarramax32@users.noreply.github.com>
2025-04-22 21:47:56 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,41 +1,36 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 8
+resource "aws_elastic_beanstalk_application" "app" {
+  name        = "mi-app-node"
+  description = "Node.js Application"
 }
 
 resource "aws_s3_bucket" "beanstalk_bucket" {
-  bucket = "mi-app-node-beanstalk-${random_id.bucket_id.hex}"
+  bucket = "mi-app-node-beanstalk-bucket"
 }
 
 resource "aws_s3_object" "app_zip" {
   bucket = aws_s3_bucket.beanstalk_bucket.bucket
   key    = "app-version.zip"
-  source = "./app-version.zip"  # Asegúrate de que esta ruta sea correcta
-}
-
-resource "aws_elastic_beanstalk_application" "app" {
-  name        = "mi-app-node"
-  description = "Mi aplicación Node.js"
+  source = "app/app-version.zip"  # Ruta al archivo ZIP dentro de la carpeta 'app'
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = "mi-app-node-env"
   application         = aws_elastic_beanstalk_application.app.name
-  solution_stack      = "64bit Amazon Linux 2 v3.3.6 running Node.js 18"
+  environment_name    = "mi-app-node-env"
   version_label       = "v1"
-  s3_bucket           = aws_s3_bucket.beanstalk_bucket.bucket
-  s3_key              = aws_s3_object.app_zip.key
 
-  setting {
+  application_version {
+    bucket = aws_s3_bucket.beanstalk_bucket.bucket
+    key    = aws_s3_object.app_zip.key
+  }
+
+  settings {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
     value     = "4"
   }
 
-  setting {
+  settings {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
     value     = "1"
