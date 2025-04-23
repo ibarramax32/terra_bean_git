c086b2ab8b54fbff8f5829db8ccadef9db48d3e4
Maximiliano Ibarra <74162048+ibarramax32@users.noreply.github.com>
2025-04-22 22:31:08 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "beanstalk_bucket" {
-  bucket = "mi-app-node-beanstalk-bucket-unique"
+  bucket = "mi-app-node-beanstalk-bucket-${random_id.bucket_suffix.hex}"
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_policy" "beanstalk_bucket_policy" {
@@ -26,16 +31,14 @@ resource "aws_elastic_beanstalk_application" "app" {
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
-  name        = "mi-app-node-env"
-  application = aws_elastic_beanstalk_application.app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v5.6.1 running Tomcat 9 Corretto 11" # Actualiza con la solución correcta
-  tier        = "WebServer"
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "ENV_VAR"
-    value     = "value"
+  name                = "mi-app-node-env"
+  application         = aws_elastic_beanstalk_application.app.name
+  solution_stack      = "64bit Amazon Linux 2 v5.6.1 running Node.js 18"
+  tier                = "WebServer"
+  option_settings {
+    namespace   = "aws:elasticbeanstalk:application:environment"
+    option_name = "ENV_VAR"
+    value       = "value"
   }
 }
-
 
