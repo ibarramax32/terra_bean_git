3602e4aa58347eff920f0a1a4720d1d6e064127c
Maximiliano Ibarra <74162048+ibarramax32@users.noreply.github.com>
2025-04-22 22:38:14 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -7,8 +7,17 @@ resource "random_id" "bucket_suffix" {
 }
 
 resource "aws_s3_bucket" "beanstalk_bucket" {
-  bucket = "${var.project_name}-beanstalk-bucket-${random_id.bucket_suffix.hex}"
+  bucket = "mi-app-node-beanstalk-bucket-${random_id.bucket_suffix.hex}"
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "beanstalk_bucket_public_access" {
+  bucket = aws_s3_bucket.beanstalk_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "beanstalk_bucket_policy" {
@@ -31,9 +40,10 @@ resource "aws_elastic_beanstalk_application" "app" {
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
-  name                = "${var.project_name}-env"
+  name                = "mi-app-node-env"
   application         = aws_elastic_beanstalk_application.app.name
-  solution_stack_name = "64bit Amazon Linux 2 v5.6.1 running Node.js 18"
+  solution_stack_name = "64bit Amazon Linux 2023 v5.6.1 running Tomcat 9 Corretto 11"
   tier                = "WebServer"
 }
+
 
